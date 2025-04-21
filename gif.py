418dead265287b8e18c90e8a5a4dc3a9f6a1e3bd
Rashid Khan <github.fiji@cspo.top>
2025-04-20 21:32:24 -0600

    Fixed memory leaks

--- gif.py
+++ gif.py
@@ -6,7 +6,7 @@ def read_blockstream(f):
         size = f.read(1)[0]
         if size == 0:
             break
-        for i in range(size):
+        for _ in range(size):
             yield f.read(1)[0]
 
 class EndOfData(Exception):
@@ -21,9 +21,9 @@ class LZWDict:
         self.clear()
 
     def clear(self):
+        self.codes = []
         self.last = b''
         self.code_len = self.code_size + 1
-        self.codes[:].clear()
 
     def decode(self, code):
         if code == self.clear_code:
@@ -37,11 +37,10 @@ class LZWDict:
             value = self.codes[code - self.end_code - 1]
         else:
             value = self.last + self.last[0:1]
-        if self.last:
+        if self.last and len(self.codes) < 4096:
             self.codes.append(self.last + value[0:1])
-        if (len(self.codes) + self.end_code + 1 >= 1 << self.code_len and
-            self.code_len < 12):
-                self.code_len += 1
+        if (len(self.codes) + self.end_code + 1 >= 1 << self.code_len and self.code_len < 12):
+            self.code_len += 1
         self.last = value
         return value
 
@@ -60,7 +59,6 @@ def lzw_decode(data, code_size):
                         bit = 0
                         byte = next(data)
                 yield dictionary.decode(code)
-
         except EndOfData:
             while True:
                 next(data)
@@ -68,47 +66,49 @@ def lzw_decode(data, code_size):
         pass
 
 class Extension:
-    def __init__(self,f):
+    def __init__(self, f):
         self.type = f.read(1)[0]
-        # 0x01 = label, 0xfe = comment
         self.data = bytes(read_blockstream(f))
-        print('extended', self.data)
-
+        gc.collect()
 
 class Frame:
-    def __init__(self, f, bitmap, palette, colors, delay):
-        self.bitmap_class = bitmap
-        self.palette_class = palette
+    def __init__(self, f, bitmap_class, palette_class, colors, delay):
         self.delay = delay
-        self.x, self.y, self.w, self.h, flags = (
-            struct.unpack('<HHHHB', f.read(9)))
+        self.x, self.y, self.w, self.h, flags = struct.unpack('<HHHHB', f.read(9))
+
         self.palette_flag = (flags & 0x80) != 0
         self.interlace_flag = (flags & 0x40) != 0
-        self.sort_flag = (flags & 0x20) != 0
         self.palette_size = 1 << ((flags & 0x07) + 1)
+
+        self.bitmap_class = bitmap_class
+        self.palette_class = palette_class
+
         if self.palette_flag:
             self.read_palette(f)
             colors = self.palette_size
+        else:
+            self.palette = None  # fallback to global later
+
         self.min_code_sz = f.read(1)[0]
         self.bitmap = self.bitmap_class(self.w, self.h, colors)
+
         x = 0
         y = 0
-
-        print('> Decode ', gc.mem_free())
         for decoded in lzw_decode(read_blockstream(f), self.min_code_sz):
-            #gc.collect()
             for byte in decoded:
-                self.bitmap[x, y] = byte
-                x += 1
-                if (x >= self.w):
-                    x = 0
-                    y += 1
-        print(' < Decode', gc.mem_free())
+                if y < self.h:
+                    self.bitmap[x, y] = byte
+                    x += 1
+                    if x >= self.w:
+                        x = 0
+                        y += 1
+        gc.collect()
 
     def read_palette(self, f):
         self.palette = self.palette_class(self.palette_size)
         for i in range(self.palette_size):
-            self.palette[i] = f.read(3)
+            rgb = f.read(3)
+            self.palette[i] = (rgb[0] << 16) | (rgb[1] << 8) | rgb[2]
 
 class GIFImage:
     def __init__(self, f, bitmap, palette):
@@ -118,43 +118,48 @@ class GIFImage:
         if self.palette_flag:
             self.read_palette(f)
         self.has_more_frames = True
-    
+        self.frame = None
+
     def read_next_frame(self, f):
-        if self.has_more_frames != True:
+        if not self.has_more_frames:
             return
-        
+
         delay = 0
         while True:
             block_type = f.read(1)[0]
-            if block_type == 0x21: # This preceeds the frame. We're looking for extension 249 to get the delay
+            if block_type == 0x21:
                 extension = Extension(f)
-                if extension.type == 249:
-                    delay = extension.data[1] * 10 # GIF encoding uses hundredths of a second, but everyone else uses thousandths 
-
-            elif block_type == 0x2c: # Image Descriptor / Image Separator  
+                if extension.type == 0xF9:  # Graphic Control Extension
+                    delay = extension.data[1] * 10  # in milliseconds
+                del extension
+                gc.collect()
+            elif block_type == 0x2C:
                 self.frame = Frame(f, self.bitmap_class, self.palette_class,
-                          self.palette_size, delay)
+                                   self.palette_size, delay)
+                if self.frame.palette is None:
+                    self.frame.palette = self.palette  # fallback to global
                 break
-            elif block_type == 0x3b: # Trailer indicating the end of the GIF Data Stream
+            elif block_type == 0x3B:
                 self.has_more_frames = False
                 break
             else:
-                pass
-                #raise ValueError('Bad block {0:2x}'.format(block_type))
+                f.read(1)  # skip unknown label
+                for _ in read_blockstream(f):
+                    pass
+                gc.collect()
         return delay
 
     def read_palette(self, f):
         self.palette = self.palette_class(self.palette_size)
         for i in range(self.palette_size):
-            self.palette[i] = f.read(3)
+            rgb = f.read(3)
+            self.palette[i] = (rgb[0] << 16) | (rgb[1] << 8) | rgb[2]
 
     def read_header(self, f):
         header = f.read(6)
         if header not in {b'GIF87a', b'GIF89a'}:
-            raise ValueError("Not GIF file")
-        self.w, self.h, flags, self.background, self.aspect = (
-            struct.unpack('<HHBBB', f.read(7)))
+            raise ValueError("Not a valid GIF")
+        self.w, self.h, flags, self.background, self.aspect = struct.unpack('<HHBBB', f.read(7))
         self.palette_flag = (flags & 0x80) != 0
-        self.sort_flag = (flags & 0x08) != 0
         self.color_bits = ((flags & 0x70) >> 4) + 1
         self.palette_size = 1 << ((flags & 0x07) + 1)
